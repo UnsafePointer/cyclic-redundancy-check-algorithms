f61227cc7e3a973be3c086b8c90c493cc2c1670a
Renzo Crisostomo <renzo@crisostomo.me>
2019-09-15 00:39:42 +0200

    Add extended comment to polynomial binary form

--- crc/crc8.go
+++ crc/crc8.go
@@ -18,7 +18,10 @@ func generateCRC8Table(polynomial byte) [256]byte {
 }
 
 func CRC8(bytes []byte) byte {
-	const polynomial = byte(0xF1) // Polynomial: (x^8 +) x^7 + x^6 + x^5 + x^4 + 1
+	// Polynomial: (x^8 +) x^7 + x^6 + x^5 + x^4 + 1
+	// MSB is implicitely 1 so (x^8 +) is ignored from
+	// the binary form
+	const polynomial = byte(0xF1)
 
 	table := generateCRC8Table(polynomial)
 	crc := byte(0)
