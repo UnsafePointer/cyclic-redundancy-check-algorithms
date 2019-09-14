5be9472627e392ce75d87baf350a98abf78b54fb
Renzo Crisostomo <renzo@crisostomo.me>
2019-09-15 00:29:21 +0200

    Add dynamically pre-calculated table
    This should speed up operations for very large slices of bytes

--- crc/crc8.go
+++ crc/crc8.go
@@ -1,18 +1,30 @@
 package crc
 
+func generateCRC8Table(polynomial byte) [256]byte {
+	table := [256]byte{}
+	for i := 0; i < 256; i++ {
+		currentByte := byte(i)
+		for i := 0; i < 8; i++ {
+			if (currentByte & 0x80) != 0 {
+				currentByte = (currentByte << 1) ^ polynomial
+			} else {
+				currentByte <<= 1
+			}
+		}
+		table[i] = currentByte
+	}
+
+	return table
+}
+
 func CRC8(bytes []byte) byte {
 	const polynomial = byte(0xF1) // Polynomial: (x^8 +) x^7 + x^6 + x^5 + x^4 + 1
 
+	table := generateCRC8Table(polynomial)
 	crc := byte(0)
 	for _, currentByte := range bytes {
-		crc ^= currentByte
-		for i := 0; i < 8; i++ {
-			if (crc & 0x80) != 0 {
-				crc = (crc << 1) ^ polynomial
-			} else {
-				crc <<= 1
-			}
-		}
+		data := currentByte ^ crc
+		crc = table[data]
 	}
 
 	return crc
